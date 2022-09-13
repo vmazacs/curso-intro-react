ea6d94e2ba21aa8ca9a662f90fdb865467bf3e9c
José Vidal Maza Alonso <vmaza@compusoluciones.com>
2022-09-12 21:30:20 -0500

    Adding completeTodo() and deleteTodo() functions in App.js, setting onComplete and onDelete props for TodoItem components in App.js. Modifying code in TodoItem.js file setting onClick methods as props.onComplete and props.onDelete.

--- src/App.js
+++ src/App.js
@@ -31,6 +31,26 @@ function App() {
     })
   }
 
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+    /*todos[todoIndex] = {
+      text: todos[todoIndex].text,
+      completed: true,
+    }*/
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
   return (
 
     <React.Fragment>
@@ -50,6 +70,8 @@ function App() {
             key={todo.text} 
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
             />
         ))}
         </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,20 +2,13 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-    const onComplete = () => {
-        alert('Ya completaste el To Do ' + props.text);
-    };
-
-    const onDelete = () => {
-        alert('Borraste el To Do ' + props.text);
-    };
 
     return (
 
         <li className="TodoItem">
             <span 
                 className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-                onClick={onComplete}
+                onClick={props.onComplete}
             >
                 √
             </span>
@@ -24,7 +17,7 @@ function TodoItem(props) {
             </p>
             <span 
                 className="Icon Icon-delete"
-                onClick={onDelete}
+                onClick={props.onDelete}
             >
                 X
             </span>
